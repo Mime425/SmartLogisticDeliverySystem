3616343a5487b171036684c672c61e8994b50d83
ctrl-thanush004 <thanushrv14@gmail.com>
2026-04-12 23:29:34 -0400

    added driver, loader and package classes, created the main menu

--- Driver.cs
+++ Driver.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SmartLogisticsDelieverySystem
 {
-    internal class Driver
+    public abstract class Driver : Worker
     {
+        private string licenseType;
+
+        public Driver(string name, int id, int experienceYears, int taskCompleted, bool isAvailable, string licenseType) : base(name, id, experienceYears, taskCompleted, isAvailable)
+        {
+            this.licenseType = licenseType;
+        }
+
+        public override void PerformTask()
+        {
+            Console.WriteLine($"{name} is delivering deliveries with a {licenseType} license");
+            AddTask();
+        }
     }
+
 }
--- Loader.cs
+++ Loader.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SmartLogisticsDelieverySystem
 {
-    internal class Loader
+    public abstract class Loader : Worker
     {
+        private double maxLiftWeight;
 
+        protected Loader(double maxLiftWeight, string name, int id, int experienceYears, int taskCompleted, bool isAvailable) : base(name, id, experienceYears, taskCompleted, isAvailable)
+        {
+            this.maxLiftWeight = maxLiftWeight;
+        }
+
+        public override void PerformTask()
+        {
+            Console.WriteLine($"{name} is loading packages up to {maxLiftWeight} kg");
+            AddTask();
+        }
     }
 }
