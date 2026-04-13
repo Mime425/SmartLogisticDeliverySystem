01a96a7e64262c2a1646cb25b06086271150d780
Mime Uddin Reba <mimereba@gmail.com>
2026-04-13 01:07:38 -0400

    touched up on package waiting syst for CustomQueue implementatoin

--- DeliverySystem.cs
+++ DeliverySystem.cs
@@ -10,15 +10,34 @@ namespace SmartLogisticsDelieverySystem
     {
         private List<Warehouse> warehouses = new List<Warehouse>();
         private List<Package> packages = new List<Package>();
+        //for package waiting system 
+        private CustomQueue<Package> waitingPackages = new CustomQueue<Package>(20);
+
 
         public void AddWarehouse(Warehouse w)
         {
             warehouses.Add(w);
         }
 
+        //methods for package waiting system from the customQueue<T>
+        //for pkg waiting syt
         public void AddPackage(Package p)
         {
             packages.Add(p);
+            waitingPackages.Enqueue(p); //add pkg enqueue
+        }
+
+        public Package NextPackage()
+        {
+            return waitingPackages.Dequeue(); //gets next package
+        }
+
+        public Package PeekNextPackage()
+            { return waitingPackages.Peek(); } //peek sees pkg
+
+        public bool EmptyPackages()
+        { 
+            return waitingPackages.IsEmpty(); //validates if its empty 
         }
 
         public Package SearchPackageById(int id)
